3d4269dcf1e38511cfa8bd498f7eae42a5e7ea50
Ismail <ismail.yimer.h@gmail.com>
2023-03-02 14:20:28 +0300

    exported new components

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,20 @@ import Header from "./Header";
 import NavBar from "./NavBar";
 import Breadcrumb from "./Breadcrumb";
 import Footer from "./Footer";
+import ProductCard from "./ProductCard";
+import OrderCard from "./OrderCard";
+import CartItem from "./CartItem";
+import OrderSummary from "./OrderSummary";
+import CheckoutCard from "./CheckoutCard";
 
-export { Header, NavBar, Breadcrumb, Footer };
+export {
+    Header,
+    NavBar,
+    Breadcrumb,
+    Footer,
+    ProductCard,
+    OrderCard,
+    CartItem,
+    OrderSummary,
+    CheckoutCard,
+};
