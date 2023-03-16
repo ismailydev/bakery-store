067eb3d8b8a85fdfee14dfb35125f4f4ee48eb13
Ismail <ismail.yimer.h@gmail.com>
2023-03-16 15:28:15 +0300

    navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,37 @@
+import { logo, shoppingBag, user } from "../constants";
+
 export default function NavBar() {
-    return <div>NavBar</div>;
+    return (
+        <div className="py-4 shadow-md">
+            <div className="max-w-6xl mx-auto flex justify-between items-center">
+                <div className="w-32">
+                    <img src={logo} alt="I&I Rose Garden" className="w-7 h-7" />
+                </div>
+                <div className="flex-1">
+                    <select name="" id="" className="flex items-center">
+                        <option value="Shop">Shop</option>
+                        <option value="Sell">Sell</option>
+                        <option value="Products">Products</option>
+                    </select>
+                </div>
+                <div className="flex gap-12">
+                    <div className="relative text-white">
+                        <img
+                            src={shoppingBag}
+                            alt="shopping bag icon"
+                            className="w-5 h-5"
+                        />
+                        <div className="absolute -top-1 -right-1 w-3 h-3 rounded-full p-[6px] flex items-center justify-center bg-accent text-[8px] border border-white">
+                            3
+                        </div>
+                    </div>
+                    <img
+                        src={user}
+                        alt="user icon"
+                        className="w-[22px] h-[22px] p-0.5 rounded-full border-[1.5px] border-black"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
