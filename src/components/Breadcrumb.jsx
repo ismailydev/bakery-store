dfc745ef71bf3a963869bc3ab949b179fe0e16c4
Ismail <ismail.yimer.h@gmail.com>
2023-03-21 14:14:19 +0300

    adjusted breadcrumb based on path

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Breadcrumb() {
     const location = useLocation();
@@ -6,19 +6,49 @@ export default function Breadcrumb() {
     const getPath = (path) => {
         switch (path) {
             case "/":
-                return ["Home", "Products"];
+                return getBreadcrumbs([
+                    { name: "Home", link: "/" },
+                    { name: "Products", link: "/" },
+                ]);
             case "/checkout":
-                return ["Product", "Shopping Cart", "Checkout"];
+                return getBreadcrumbs([
+                    { name: "Product", link: "/" },
+                    { name: "Shopping Cart", link: "/cart" },
+                    { name: "Checkout", link: "/checkout" },
+                ]);
             case "/cart":
-                return ["Product", "Shopping Cart"];
+                return getBreadcrumbs([
+                    { name: "Product", link: "/" },
+                    { name: "Shopping Cart", link: "/cart" },
+                ]);
             case "/account":
-                return ["Home", "Account"];
+                return getBreadcrumbs([
+                    { name: "Home", link: "/" },
+                    { name: "Account", link: "/account" },
+                ]);
         }
     };
 
+    const getBreadcrumbs = (arr) => {
+        return (
+            <div className="flex">
+                {arr.map((item, i) => (
+                    <div className="flex">
+                        <Link to={item.link} key={i}>
+                            <span className={`${i === 0 ? "hidden" : ""} mx-5`}>
+                                |
+                            </span>
+                            {item.name}
+                        </Link>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
     return (
-        <div className="py-5 max-w-6xl mx-auto text-tertiary text-sm">
-            {getPath(location.pathname).join(" | ")}
+        <div className="pt-12 max-w-6xl mx-auto text-tertiary text-sm">
+            {getPath(location.pathname)}
         </div>
     );
 }
