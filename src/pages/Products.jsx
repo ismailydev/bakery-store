926de6bd5cb46b20f12663060075b3dc8d9e4ecc
Ismail <ismail.yimer.h@gmail.com>
2023-03-22 14:15:27 +0300

    products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,26 @@
+import { ProductCard } from "../components";
+import { data as products } from "../constants";
+
 export default function Products() {
-    return <div>Products</div>;
+    return (
+        <div className="min-h-screen py-12">
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-bold">All Products</h1>
+                <div className="flex items-center">
+                    <p>Sort By:</p>
+                    <select name="" id="">
+                        <option value="Newest">Newest</option>
+                        <option value="Popular">Popular</option>
+                        <option value="Lowest Price">Lowest Price</option>
+                        <option value="Highest Price">Highest Price</option>
+                    </select>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-5 py-12">
+                {products.map((product) => (
+                    <ProductCard key={product.id} {...product} />
+                ))}
+            </div>
+        </div>
+    );
 }
