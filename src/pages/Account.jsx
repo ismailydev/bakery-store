615547d943d0a45b4b463fdc17d58b809bc73481
Ismail <ismail.yimer.h@gmail.com>
2023-03-25 14:18:15 +0300

    account page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,3 +1,17 @@
+import { OrderCard } from "../components";
+import { data as products } from "../constants";
+
 export default function Account() {
-    return <div>Account</div>;
+    return (
+        <div className="min-h-screen py-12">
+            <div className="flex justify-between">
+                <h1 className="text-3xl font-bold">My Orders</h1>
+            </div>
+            <div className="grid grid-cols-1 gap-5 py-8 pb-24">
+                {products.map((product) => (
+                    <OrderCard key={product.id} {...product} />
+                ))}
+            </div>
+        </div>
+    );
 }
