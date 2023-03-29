9e7b624b9974f0ebeac538252987e644453b629c
Ismail <ismail.yimer.h@gmail.com>
2023-03-29 15:19:25 +0300

    switched order to account

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Products from "./Products";
 import Cart from "./Cart";
 import Checkout from "./Checkout";
-import Orders from "./Orders";
+import Account from "./Account";
 
-export { Products, Cart, Checkout, Orders };
+export { Products, Cart, Checkout, Account };
