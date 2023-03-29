f7bc6d5dc156bef5ad732931a90ac81ec1d71cc7
Ismail <ismail.yimer.h@gmail.com>
2023-03-29 11:32:43 +0300

    added all pages to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Products, Cart, Checkout, Orders } from "./pages";
+import { Header, NavBar, Footer } from "./components";
+
 function App() {
     return (
-        <div className="App">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </div>
+        <BrowserRouter>
+            <Header />
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Products />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/checkout" element={<Checkout />} />
+                <Route path="/orders" element={<Orders />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     );
 }
 
