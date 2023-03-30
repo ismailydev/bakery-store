90fc546c5c6a876ccc93f494ee04d236409e0e41
Ismail <ismail.yimer.h@gmail.com>
2023-03-30 14:24:26 +0300

    fixed page icon, added json data

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+import image1 from "../assets/images/Rectangle 34.jpg";
+import image2 from "../assets/images/Rectangle 34 (1).jpg";
+import image3 from "../assets/images/Rectangle 34 (2).jpg";
+import image4 from "../assets/images/Rectangle 34 (3).jpg";
+import image5 from "../assets/images/Rectangle 34 (4).jpg";
+import image6 from "../assets/images/Rectangle 34 (5).jpg";
+
+import logo from "../assets/icons/II_LOGO 3.png";
+import facebook from "../assets/icons/facebook (1).png";
+import facebookG from "../assets/icons/facebook (2).png";
+import instagram from "../assets/icons/instagram (1).png";
+import instagramG from "../assets/icons/instagram (2).png";
+import twitter from "../assets/icons/twitter (1).png";
+import twitterG from "../assets/icons/twitter (2).png";
+import user from "../assets/icons/user.png";
+import userCircle from "../assets/icons/Group 1.png";
+import layoutList from "../assets/icons/layout-list.png";
+import logOut from "../assets/icons/log-out.png";
+import phone from "../assets/icons/phone.png";
+import shoppingBag from "../assets/icons/shopping-bag.png";
+import truck from "../assets/icons/truck.png";
+
+import data from "./data";
+
+export {
+    logo,
+    facebook,
+    facebookG,
+    twitter,
+    twitterG,
+    instagram,
+    instagramG,
+    user,
+    userCircle,
+    layoutList,
+    logOut,
+    phone,
+    shoppingBag,
+    truck,
+};
+
+export { image1, image2, image3, image4, image5, image6 };
+
+export { data };
